53621a77f139d0c54eee1e56bd2158748e69f333
juanceresa <jcere@umich.edu>
2025-07-30 21:21:33 -0500

    about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -13,6 +13,8 @@ import ReactIcon from "@/assets/icons/react.svg";
 import ChromeIcon from "@/assets/icons/chrome.svg";
 import GithubIcon from "@/assets/icons/github.svg";
 import dynamic from "next/dynamic";
+import { CardHeader } from "@/components/CardHeader";
+import { ToolboxItems } from "@/components/TollboxItems";
 
 const BentoItemMapLocation = dynamic(() => import("@/components/MapLocation"), {
 	ssr: false,
@@ -47,60 +49,55 @@ const toolboxItems = [
 
 export const AboutSection = () => {
 	return (
-		<div className="pb-96">
+		<div className="py-20 lg:py-28">
 			<div className="container">
 				<SectionHeader
 					eyebrow="About Me"
 					title="A Glimpse into My World"
 					description="Learn more about who I am, what I do, and what inspires me."
 				/>
-				<div className="mt-20">
-					<Card className="h-[320px]">
-						<div className="flex flex-col">
-							<div className="inline-flex items-center gap-2">
-								<StarIcon className="size-9 text-yellow-400" />
-								<h3 className="font-serif tex-3xl">My Reads</h3>
+				<div className="mt-20 flex flex-col gap-8">
+					<div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+						<Card className="h-[320px] md:col-span-2 lg:col-span-1">
+							<CardHeader
+								title="My Reads"
+								description="Explore the books shaping my perspectives."
+							/>
+							<div className="w-40 mx-auto mt-2 md:mt-0">
+								<Image src={bookImage} alt="Book cover" />
 							</div>
-							<p className="text-sm text-white/60 mt-2">
-								Explore the books shaping my perspectives.
-							</p>
-						</div>
-						<div className="w-40 mx-auto mt-8">
-							<Image src={bookImage} alt="Book cover" />
-						</div>
-					</Card>
+						</Card>
 
-					<Card>
-						<div>
-							<StarIcon />
-							<h3>My Toolbox</h3>
-							<p>
-								Explore the technologies and tools I use to craft digital
-								experiences.
-							</p>
-						</div>
-						<div>
-							{toolboxItems.map((item) => (
-								<div key={item.title}>
-									<TechIcon component={item.iconType} />
-									<span>{item.title}</span>
-								</div>
-							))}
-						</div>
-					</Card>
-					<Card>
-						<div>
-							<StarIcon />
-							<h3>Beyond the Code</h3>
-							<p>
-								Explore my interests, hobbies, and the things that inspire me
-								outside of coding.
-							</p>
-						</div>
-					</Card>
-					<Card className="h-64">
-						<BentoItemMapLocation className="rounded-3xl" />
-					</Card>
+						<Card className="h-[320px] md:col-span-3 lg:col-span-2">
+							<CardHeader
+								title="My Toolbox"
+								description="Explore the technologies and tools I use to craft digital
+								experiences."
+								className=""
+							/>
+
+							<ToolboxItems items={toolboxItems} className="" />
+							<ToolboxItems
+								items={toolboxItems}
+								className="mt-6"
+								itemsWrapperClassName="-translate-x-1/2"
+							/>
+						</Card>
+					</div>
+
+					<div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
+						<Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+							<BentoItemMapLocation className="rounded-3xl" />
+						</Card>
+
+						<Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+							<CardHeader // PLACEHOLDER -- MAYBE GITHUB STATS CARD
+								title="Beyond the Code"
+								description="Explore my interests, hobbies, and the things that inspire me
+								outside of coding."
+							/>
+						</Card>
+					</div>
 				</div>
 			</div>
 		</div>
