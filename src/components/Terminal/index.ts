e150ed42ba5a5f2757e5d3f309e49d2ecede380f
juanceresa <jcere@umich.edu>
2025-08-01 14:31:42 -0500

    deploy good

--- src/components/Terminal/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Terminal } from './Terminal';
-export { default as TerminalBody } from './TerminalBody';
-export { default as TerminalExample } from './TerminalExample';
-export type { TerminalLine } from './TerminalBody';
-export type { TerminalProps } from './Terminal';
